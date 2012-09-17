4721d765ad070513f2abcaa93a072246d3526066
duelgamer <duelgamer@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-09-17 16:31:01 +0000

    Created Screen class. (GH)

--- trunk/main/Game.java
+++ trunk/main/Game.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,15 +10,19 @@ import javax.swing.SwingUtilities;
 @SuppressWarnings("serial")
 public class Game extends JFrame {
 	private static GamePanel gamePanel;
+	private Screen activeScreen;
+	private int width = 500, height = 500;
 	
 	public Game(){
 		super("Space Trader");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		gamePanel = new GamePanel();
-		gamePanel.setPreferredSize(new Dimension(500,500));
+		gamePanel.setPreferredSize(new Dimension(width, height));
 		getContentPane().add(gamePanel);
 		
+		setLocation((int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth()/2 - width/2), 
+	    		(int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight()/2 - height/2));
 		setResizable(false);
 		pack();
 		setVisible(true);
