297abc566c4399d579a221bb12d0726505b0e52c
kenisha.luby@gmail.com <kenisha.luby@gmail.com@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-09-27 21:37:56 +0000

    M4 calc method for person 3. (KSL)

--- M4/src/edu/gatech/oad/antlab/person/Person3.java
+++ M4/src/edu/gatech/oad/antlab/person/Person3.java
@@ -4,7 +4,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  reversed string 
  *  
- *  @author  Bob
+ *  @author  Kenisha Luby
  *  @version 1.1
  */
 public class Person3 {
@@ -42,7 +42,11 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
+		String ans = "";
+		for(int i = input.length() - 1; i >= 0; i--){
+			ans += input.charAt(i);
+		}
 	  //Person 3 put your implementation here
-	  return null;
+	  return ans;
 	}
 }
