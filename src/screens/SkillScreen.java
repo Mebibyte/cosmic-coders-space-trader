7dcf7494b1c0a65201a1e156473e4f966cec3c91
duelgamer <duelgamer@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-09-29 03:40:23 +0000

    Added skill point used indicators. (GH)

--- src/screens/SkillScreen.java
+++ src/screens/SkillScreen.java
@@ -10,12 +10,16 @@ import java.awt.Graphics;
 import java.awt.Point;
 import java.util.Arrays;
 
+import javax.swing.ImageIcon;
+
 import main.GamePanel;
 
 public class SkillScreen extends Screen{
     private SkillButton[] buttons;
     
     private int[] skills;
+    
+    private final ImageIcon unusedSkillPoint, usedSkillPoint;
 	
     private int width, height;
 	
@@ -24,19 +28,20 @@ public class SkillScreen extends Screen{
 	public SkillScreen(GamePanel panel, int width, int height){
 	    skills = new int[3];
 	    buttons = new SkillButton[6];
-	    int xNeg = 300;
-	    int xPos = 650;
-	    int yNeg = 250;
-	    int yPos = 250;
-	    buttons[0] = new SkillButton("-", xNeg, yNeg);
-	    buttons[1] = new SkillButton("+", xPos, yPos);
-	    buttons[2] = new SkillButton("-", xNeg, yNeg + 50);
-        buttons[3] = new SkillButton("+", xPos, yPos + 50);
-        buttons[4] = new SkillButton("-", xNeg, yNeg + 100);
-        buttons[5] = new SkillButton("+", xPos, yPos + 100);
+	    int xNeg = (width / 2) - 90;
+	    int xPos = (width / 2) + 55;
+	    int y = (width / 2);
+	    buttons[0] = new SkillButton("-", xNeg, y);
+	    buttons[1] = new SkillButton("+", xPos, y);
+	    buttons[2] = new SkillButton("-", xNeg, y + 50);
+        buttons[3] = new SkillButton("+", xPos, y + 50);
+        buttons[4] = new SkillButton("-", xNeg, y + 100);
+        buttons[5] = new SkillButton("+", xPos, y + 100);
 		this.panel = panel;
 		this.width = width;
 		this.height = height;
+		unusedSkillPoint = new ImageIcon(getClass().getResource("/res/unusedSkillPoint.png"));
+		usedSkillPoint = new ImageIcon(getClass().getResource("/res/usedSkillPoint.png"));
 	}
 
 	@Override
@@ -44,6 +49,20 @@ public class SkillScreen extends Screen{
 		g.drawString("Skill Screen", 200, 150);
 		for (int i = 0; i < buttons.length; i++) {
 		    buttons[i].draw(g, panel, width, height);
+		}
+		int x = width / 2 - 50;
+		int y = 255;
+		for (int skill = 0; skill < skills.length; skill++) {
+		    for (int i = 0; i < skills[skill]; i++) {
+		        usedSkillPoint.paintIcon(panel, g, x, y);
+		        x += 10;
+		    }
+		    for (int j = skills[skill]; j < 10; j++) {
+                unusedSkillPoint.paintIcon(panel, g, x, y);
+                x+=10;
+            }
+		    y += 50;
+		    x = width / 2 - 50;
 		}
 	}
 
