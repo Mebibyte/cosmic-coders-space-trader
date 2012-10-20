fd8d7cb6284bc5ae1ee7e27b0fba29472c9b49e4
pconner03@gmail.com <pconner03@gmail.com@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-10-20 19:36:39 +0000

    Started market-related classes (PRC)

---
+++ trunk/src/edu/gatech/spacetrader/good/Good.java
@@ -0,0 +1,23 @@
+package edu.gatech.spacetrader.good;
+
+/**
+ * Will probably only be used statically. 
+ * Contains information about good types and what affects them
+ * @author Patrick Conner
+ * @version 1.0
+ * 
+ */
+public class Good {
+	
+	
+	
+	public static enum GoodTypes{
+		WATER, FURS, ORE, MEDICINE, MACHINES, WEAPONS, GOLD, NARCOTICS, PRODUCE, MEAT 
+	}
+	
+	public static final int WATER_BASEPRICE = 30;
+	public static final int FURS_BASEPRICE = 250;
+	//etc...
+	
+	
+}
---
+++ trunk/src/edu/gatech/spacetrader/good/PlanetMarket.java
@@ -0,0 +1,125 @@
+package edu.gatech.spacetrader.good;
+
+import edu.gatech.spacetrader.planet.Planet;
+
+/**
+ * 
+ * Generates prices of goods for a planet based on its conditions
+ * @author Patrick Conner
+ * @version 1.0
+ */
+public class PlanetMarket {
+	private int waterSellPrice;
+	private int fursSellPrice;
+	private int oreSellPrice;
+	private int medicineSellPrice;
+	private int machinesSellPrice;
+	private int weaponsSellPrice;
+	private int goldSellPrice;
+	private int narcoticsSellPrice;
+	private int produceSellPrice;
+	private int meatSellPrice;
+	
+	private int waterBuyPrice;
+	private int fursBuyPrice;
+	private int oreBuyPrice;
+	private int medicineBuyPrice;
+	private int machinesBuyPrice;
+	private int weaponsBuyPrice;
+	private int goldBuyPrice;
+	private int narcoticsBuyPrice;
+	private int produceBuyPrice;
+	private int meatBuyPrice;
+	
+	// getters and setters. Lots of them
+	
+	
+	public int getWaterSellPrice() {
+		return waterSellPrice;
+	}
+
+	public int getFursSellPrice() {
+		return fursSellPrice;
+	}
+
+	public int getOreSellPrice() {
+		return oreSellPrice;
+	}
+
+	public int getMedicineSellPrice() {
+		return medicineSellPrice;
+	}
+
+	public int getMachinesSellPrice() {
+		return machinesSellPrice;
+	}
+
+	public int getWeaponsSellPrice() {
+		return weaponsSellPrice;
+	}
+
+	public int getGoldSellPrice() {
+		return goldSellPrice;
+	}
+
+	public int getNarcoticsSellPrice() {
+		return narcoticsSellPrice;
+	}
+
+	public int getProduceSellPrice() {
+		return produceSellPrice;
+	}
+
+	public int getMeatSellPrice() {
+		return meatSellPrice;
+	}
+
+	public int getWaterBuyPrice() {
+		return waterBuyPrice;
+	}
+
+	public int getFursBuyPrice() {
+		return fursBuyPrice;
+	}
+
+	public int getOreBuyPrice() {
+		return oreBuyPrice;
+	}
+
+	public int getMedicineBuyPrice() {
+		return medicineBuyPrice;
+	}
+
+	public int getMachinesBuyPrice() {
+		return machinesBuyPrice;
+	}
+
+	public int getWeaponsBuyPrice() {
+		return weaponsBuyPrice;
+	}
+
+	public int getGoldBuyPrice() {
+		return goldBuyPrice;
+	}
+
+	public int getNarcoticsBuyPrice() {
+		return narcoticsBuyPrice;
+	}
+
+	public int getProduceBuyPrice() {
+		return produceBuyPrice;
+	}
+
+	public int getMeatBuyPrice() {
+		return meatBuyPrice;
+	}
+
+	
+	
+	public PlanetMarket createPlanetMarket(Planet p){
+		//TODO Logic here that generates market prices
+		//based on environment, current event, and civilization level
+		return null;
+	}
+
+}
