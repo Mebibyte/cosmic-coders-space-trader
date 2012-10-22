2e2771aa2cc99154eba2bbd9d59c7e6ab073e1a7
duelgamer <duelgamer@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-10-22 19:35:15 +0000

    Continued work on buy prices. Changed civilization level to tech level.

--- src/edu/gatech/spacetrader/good/Good.java
+++ src/edu/gatech/spacetrader/good/Good.java
@@ -12,20 +12,21 @@ package edu.gatech.spacetrader.good;
  * 
  */
 public enum Good {
-	WATER(30), FURS(250), FOOD(100), ORE(350), GAMES(250), 
-	FIREARMS(1250), MEDICINE(650), MACHINES(900), NARCOTICS(3500), ROBOTS(5000);
+	WATER(30, 0), FURS(250, 1), FOOD(100, 2), ORE(350, 3), GAMES(250, 4), 
+	FIREARMS(1250, 5), MEDICINE(650, 6), MACHINES(900, 7), NARCOTICS(3500, 8), ROBOTS(5000, 9);
 	
 	/**
      * Field basePrice.
      */
-	private final int basePrice;
+	private final int basePrice, index;
 	
 	/**
      * Constructor for Good.
      * @param basePrice int.
      */
-	private Good(int basePrice) {
+	private Good(int basePrice, int index) {
 	    this.basePrice = basePrice;
+	    this.index = index;
 	}
 	
 	/**
@@ -34,5 +35,20 @@ public enum Good {
 	 */
     public int getBasePrice() {
         return basePrice;
+    }
+    
+    /**
+     * Method getIndex.
+     * @return int index.
+     */
+    public int getIndex() {
+        return index;
+    }
+
+    public static int getBasePrice(int i) {
+        for(Good g : Good.values()) {
+            if (g.index == i) return g.getBasePrice();
+        }
+        return 0;
     }
 }
