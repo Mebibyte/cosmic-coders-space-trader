ec8492bb22eb7f2e5622f6e0c6490010a5b62d56
pconner03@gmail.com <pconner03@gmail.com@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-10-22 16:15:33 +0000

    Finished base price definitions. Added market to Planet class (PRC)

--- trunk/src/edu/gatech/spacetrader/good/Good.java
+++ trunk/src/edu/gatech/spacetrader/good/Good.java
@@ -17,7 +17,15 @@ public class Good {
 	
 	public static final int WATER_BASEPRICE = 30;
 	public static final int FURS_BASEPRICE = 250;
-	//etc...
+	public static final int ORE_BASEPRICE = 350;
+	public static final int MEDICINE_BASEPRICE = 650;
+	public static final int MACHINES_BASEPRICE = 900;
+	public static final int WEAPONS_BASEPRICE = 1250;
+	public static final int GOLD_BASEPRICE = 500;
+	public static final int NARCOTICS_BASEPRICE = 3500;
+	public static final int PRODUCE_BASEPRICE = 100;
+	public static final int MEAT_BASEPRICE = 200;
+	
 	
 	
 }
