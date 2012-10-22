8bfe7ef6ab93da8b246c4123604cadebbc95f6bb
duelgamer <duelgamer@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-10-22 20:04:11 +0000

    CodePro fixes. (GH)

--- trunk/src/edu/gatech/spacetrader/planet/Planet.java
+++ trunk/src/edu/gatech/spacetrader/planet/Planet.java
@@ -181,10 +181,10 @@ public class Planet {
     /**
      */
     public static enum TechLevel{
-    	PRE_AGRICULTURAL(0), AGRICULTURAL(1), MEDIEVAL(2), RENAISSANCE(3), EARLY_INDUSTRIAL(4),
-    	INDUSTRIAL(5), POST_INDUSTRIAL(6), HI_TECH(7);
+    	PRE_AGRICULTURAL(0), AGRICULTURAL(1), MEDIEVAL(2), RENAISSANCE(3),
+    	EARLY_INDUSTRIAL(4), INDUSTRIAL(5), POST_INDUSTRIAL(6), HI_TECH(7);
     	
-    	private int levelInt;
+    	private final int levelInt;
     	
     	private TechLevel(int levelInt) {
     	    this.levelInt = levelInt;
