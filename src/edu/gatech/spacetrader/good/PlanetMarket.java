752223d05c9049f35c0d86b8f6fe3c973bf76a5f
pconner03@gmail.com <pconner03@gmail.com@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-10-23 01:42:12 +0000

    Cleaned up some methods in PlanetMarket so that I can finish them later (PRC)

--- src/edu/gatech/spacetrader/good/PlanetMarket.java
+++ src/edu/gatech/spacetrader/good/PlanetMarket.java
@@ -38,12 +38,35 @@ public class PlanetMarket {
 	    }
 	}
 	
+//<<<<<<< .mine
+	
+	public void createPlanetMarket(Planet p){
+		//TODO Logic here that generates market prices
+		//based on environment, current event, and civilization level
+		if(p.getCivLevel().getTechLevel() < 
+				edu.gatech.spacetrader.planet.Planet.
+				TechLevel.EARLY_INDUSTRIAL.getTechLevel()){
+			prices[Good.FIREARMS.getIndex()][BUY]=0;
+			prices[Good.FIREARMS.getIndex()][SELL]=0;
+			
+			prices[Good.ROBOTS.getIndex()][BUY]=0;
+			prices[Good.ROBOTS.getIndex()][SELL]=0;
+			/*I have NO idea if this is the right way to do this
+			 *The "index" system is confusing here.
+			 */
+	
+		}	
+	}
+//=======
 	public String toString(){
+		//Instead of doing a toString like this, maybe we can format it with HTML
+		//into a table (if that works for Graphics) to show on the market screen
 	    final StringBuffer ans = new StringBuffer("[");
 	    for (int i = 0; i < prices.length; i++) {
 	        ans.append(Arrays.toString(prices[i]));
 	    }
 	    ans.append(']');
 	    return ans.toString();
+//>>>>>>> .r118
 	}
 }
