b156631674ecdbebba845867e8a30bf1a850f198
piecappe <piecappe@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-10-24 01:02:05 +0000

    Added all necessary inputs for Good constructor (PC)

--- src/edu/gatech/spacetrader/good/Good.java
+++ src/edu/gatech/spacetrader/good/Good.java
@@ -12,23 +12,29 @@ package edu.gatech.spacetrader.good;
  * 
  */
 public enum Good {
-	WATER(30, 0), FURS(250, 1), FOOD(100, 2), ORE(350, 3), 
-	GAMES(250, 4), FIREARMS(1250, 5), MEDICINE(650, 6),
-	MACHINES(900, 7), NARCOTICS(3500, 8), ROBOTS(5000, 9);
+	WATER(30, 0, 0, 0, 3, 4), FURS(250, 1, 0, 0, 10, 10), FOOD(100, 2, 1, 0, 20, 10), 
+	ORE(350, 3, 2, 2, 20, 10), GAMES(250, 4, 3, 1, -10, 5), 
+	FIREARMS(1250, 5, 3, 1, -75, 100), MEDICINE(650, 6, 4, 1, -20, 10),
+	MACHINES(900, 7, 4, 3, -30, 5), NARCOTICS(3500, 8, 5, 0, -125, 150), 
+	ROBOTS(5000, 9, 6, 4, -150, 100);
 	
 	/**
      * Field basePrice.
      */
-	private final int basePrice, index;
+	private final int basePrice, index, MTLP, MTLU, IPL, VAR;
 	
 	/**
      * Constructor for Good.
      * @param basePrice int.
      * @param index int.
      */
-	private Good(int basePrice, int index) {
+	private Good(int basePrice, int index, int MTLP, int MTLU, int IPL, int VAR) {
 	    this.basePrice = basePrice;
-	    this.index = index;
+	    this.index = index; 
+	    this.MTLP = MTLP; 
+	    this.MTLU = MTLU; 
+	    this.IPL = IPL; 
+	    this.VAR = VAR; 
 	}
 	
 	/**
