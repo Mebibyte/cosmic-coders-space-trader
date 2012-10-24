0a33793594a9f8ddd751b5725e826b0b2c8ab012
duelgamer <duelgamer@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-10-24 06:23:15 +0000

    Added buy & sell price to market place GUI. (GH)

--- src/edu/gatech/spacetrader/good/Good.java
+++ src/edu/gatech/spacetrader/good/Good.java
@@ -1,8 +1,10 @@
+// $codepro.audit.disable multiplicationOrDivisionByPowersOf2
 /* Comment
  * 
  */
 // $codepro.audit.disable numericLiterals
 package edu.gatech.spacetrader.good;
+
 import java.awt.Graphics;
 import java.util.Random;
 
@@ -10,85 +12,96 @@ import edu.gatech.spacetrader.main.GamePanel;
 import edu.gatech.spacetrader.planet.Planet;
 
 /**
- * Will probably only be used statically. 
- * Contains information about good types and what affects them
+ * Will probably only be used statically. Contains information about good types
+ * and what affects them
+ * 
  * @author Patrick Conner
  * @version 1.0
  * 
  */
 public class Good {
-    
+
     public static enum GoodType {
-    	WATER(30, 0, 0, 0, 3, 4), FURS(250, 1, 0, 0, 10, 10), FOOD(100, 2, 1, 0, 20, 10), 
-    	ORE(350, 3, 2, 2, 20, 10), GAMES(250, 4, 3, 1, -10, 5), 
-    	FIREARMS(1250, 5, 3, 1, -75, 100), MEDICINE(650, 6, 4, 1, -20, 10),
-    	MACHINES(900, 7, 4, 3, -30, 5), NARCOTICS(3500, 8, 5, 0, -125, 150), 
-    	ROBOTS(5000, 9, 6, 4, -150, 100);
-    	
-    	/**
-         * Field basePrice. 
-         * Field index. 
-         * Field MTLP. 
-         * Field MTLU. 
-         * Field IPL. 
-         * Field VAR. 
+        WATER(30, 0, 0, 0, 3, 4), FURS(250, 1, 0, 0, 10, 10), FOOD(100, 2, 1,
+                0, 20, 10), ORE(350, 3, 2, 2, 20, 10), GAMES(250, 4, 3, 1, -10,
+                5), FIREARMS(1250, 5, 3, 1, -75, 100), MEDICINE(650, 6, 4, 1,
+                -20, 10), MACHINES(900, 7, 4, 3, -30, 5), NARCOTICS(3500, 8, 5,
+                0, -125, 150), ROBOTS(5000, 9, 6, 4, -150, 100);
+
+        /**
+         * Field basePrice. Field index. Field MTLP. Field MTLU. Field IPL.
+         * Field VAR.
          */
-    	private final int basePrice, index, MTLP, MTLU, IPL, VAR;
-    	
-    	/**
-    	 * Good constructor.
-    	 * @param basePrice
-    	 * @param index
-    	 * @param MTLP
-    	 * @param MTLU
-    	 * @param IPL
-    	 * @param VAR
-    	 */
-    	private GoodType(int basePrice, int index, int MTLP, int MTLU, int IPL, int VAR) {
-    	    this.basePrice = basePrice;
-    	    this.index = index; 
-    	    this.MTLP = MTLP; 
-    	    this.MTLU = MTLU; 
-    	    this.IPL = IPL; 
-    	    this.VAR = VAR; 
-    	}
+        private final int basePrice, index, MTLP, MTLU, IPL, VAR;
+
+        /**
+         * Good constructor.
+         * 
+         * @param basePrice
+         * @param index
+         * @param MTLP
+         * @param MTLU
+         * @param IPL
+         * @param VAR
+         */
+        private GoodType(int basePrice, int index, int MTLP, int MTLU, int IPL,
+                int VAR) {
+            this.basePrice = basePrice;
+            this.index = index;
+            this.MTLP = MTLP;
+            this.MTLU = MTLU;
+            this.IPL = IPL;
+            this.VAR = VAR;
+        }
 
         public static GoodType getGoodType(int index) {
             for (GoodType g : GoodType.values()) {
-                if (g.index == index) return g;
+                if (g.index == index) {
+                    return g;
+                }
             }
             return null;
         }
+
+        public int getMTLU() {
+            return MTLU;
+        }
     }
-    
-    private GoodType type;
-    
+
+    private final GoodType type;
+
     private Planet planet;
-    
+
     private int quantity, buyPrice, sellPrice;
-    
-    private static final Random rand = new Random();
-    
+
+    private static final Random RAND = new Random();
+
     public Good(int index, Planet planet) {
+        this.planet = planet;
         this.type = GoodType.getGoodType(index);
-        this.buyPrice = type.basePrice;
+        this.buyPrice = type.basePrice
+                + (type.IPL * (planet.getCivLevel().getTechLevel() - type.MTLP))
+                + ((RAND.nextBoolean() ? 1 : -1) * RAND.nextInt(type.VAR));
         this.sellPrice = (int) (buyPrice * .9);
     }
-    
+
     public boolean buyGood() {
         if (quantity > 0) {
             quantity--;
             return true;
-        } else return false;
+        } else {
+            return false;
+        }
     }
-    
+
     public void addGood() {
         quantity++;
     }
-    
+
     public void updatePrice() {
-        buyPrice = type.basePrice + (type.IPL * (planet.getCivLevel().getTechLevel() - type.MTLP)) + 
-                ((rand.nextBoolean() ? 1 : -1) * rand.nextInt(type.VAR));
+        buyPrice = type.basePrice
+                + (type.IPL * (planet.getCivLevel().getTechLevel() - type.MTLP))
+                + ((RAND.nextBoolean() ? 1 : -1) * RAND.nextInt(type.VAR));
         sellPrice = (int) (this.buyPrice * .9);
     }
 
@@ -98,13 +111,26 @@ public class Good {
 
     public int getSellPrice() {
         return sellPrice;
-    } 
-    
-    public int getIndex(){
-    	return type.index; 
+    }
+
+    public int getIndex() {
+        return type.index;
     }
 
     public void draw(Graphics g, GamePanel panel, int x, int y) {
-        g.drawString(type.toString(), x + (50 / 2) - (g.getFontMetrics().stringWidth(type.toString()) / 2), y + 73);
+        g.drawString(buyPrice + "", x + (50 / 2)
+                - (g.getFontMetrics().stringWidth(buyPrice + "") / 2), y + 33);
+        g.drawString(sellPrice + "", x + (50 / 2)
+                - (g.getFontMetrics().stringWidth(sellPrice + "") / 2), y + 53);
+        g.drawString(type.toString(), x + (50 / 2)
+                - (g.getFontMetrics().stringWidth(type.toString()) / 2), y + 73);
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 }
--- src/edu/gatech/spacetrader/good/PlanetMarket.java
+++ src/edu/gatech/spacetrader/good/PlanetMarket.java
@@ -12,100 +12,105 @@ import edu.gatech.spacetrader.main.GamePanel;
 import edu.gatech.spacetrader.planet.Planet;
 
 /**
- * 
  * Generates prices of goods for a planet based on its conditions
+ * 
  * @author Patrick Conner
  * @version 1.0
  */
 public class PlanetMarket {
-    
-    private static final ImageIcon goodBG = new ImageIcon(
+
+    private static final ImageIcon GOOD_BG = new ImageIcon(
             BigButton.class.getResource("/edu/gatech/spacetrader/res/good.png"));
-    
+
     /**
-     * Field NUM_GOODS.
-     * (Value is 10)
+     * Field NUM_GOODS. (Value is 10)
      */
     private static final int NUM_GOODS = 10;
-    
+
     /**
-     * Field goods.
-     * Array of goods for PlanetMarket.
+     * Field goods. Array of goods for PlanetMarket.
      */
     private Good[] goods = new Good[NUM_GOODS];
-	
-	/**
+
+    /**
      * Constructor for PlanetMarket that sets all buy prices to base price.
      * 
-     * @param planet Planet this market resides on.
+     * @param planet
+     *            Planet this market resides on.
      */
-	public PlanetMarket(Planet planet){
-		for (int i = 0; i < goods.length; i++) {
-		    goods[i] = new Good(i, planet);
-		}
-	}
-	
-	/**
-	 * Updates prices based on several factors of the planet and good.
-	 */
-	public void updatePrices(){
-	    for (int i = 0; i < goods.length; i++) {
-	        goods[i].updatePrice();
-	    }
-	}
-	
-	/**
-	 * Get the buy price of a specified good.
-	 * 
-	 * @param g Good.
-	 * @return Price of specified good.
-	 */
-	public int getBuyPrice(Good g) {
+    public PlanetMarket(Planet planet) {
+        for (int i = 0; i < goods.length; i++) {
+            goods[i] = new Good(i, planet);
+        }
+    }
+
+    /**
+     * Updates prices based on several factors of the planet and good.
+     */
+    public void updatePrices() {
+        for (int i = 0; i < goods.length; i++) {
+            goods[i].updatePrice();
+        }
+    }
+
+    /**
+     * Get the buy price of a specified good.
+     * 
+     * @param g
+     *            Good.
+     * @return Price of specified good.
+     */
+    public int getBuyPrice(Good g) {
         return g.getBuyPrice();
     }
-	
-	/**
+
+    /**
      * Get the sell price of a specified good.
      * 
-     * @param g Good.
+     * @param g
+     *            Good.
      * @return Price of specified good.
      */
     public int getSellPrice(Good g) {
         return g.getSellPrice();
     }
-	
-	/**
-	 * draw method for PlanetMarket.
-	 * Draws the goods in the planetMarket using the given graphics g.
-	 * 
-	 * @param g Graphics to be drawn.
-	 * @param panel GamePanel.
-	 * @param x X-coordinate.
-	 * @param y Y-coordinate.
-	 */
+
+    /**
+     * draw method for PlanetMarket. Draws the goods in the planetMarket using
+     * the given graphics g.
+     * 
+     * @param g
+     *            Graphics to be drawn.
+     * @param panel
+     *            GamePanel.
+     * @param x
+     *            X-coordinate.
+     * @param y
+     *            Y-coordinate.
+     */
     public void draw(Graphics g, GamePanel panel, int x, int y) {
         g.drawRect(x, y, 250, 150);
         int goodCount = 0;
         for (int i = y; i < (y + 150); i += 75) {
             for (int j = x; j < (x + 250); j += 50) {
-                goodBG.paintIcon(panel, g, j, i);
+                GOOD_BG.paintIcon(panel, g, j, i);
                 goods[goodCount].draw(g, panel, j, i);
                 goodCount++;
             }
         }
     }
-	
-	/**
-	 * toString method for PlanetMarket required by CodePro.
-	 * 
-	 * @return String representation of PlanetMarket.
-	 */
-	public String toString(){
-	    final StringBuffer ans = new StringBuffer("[");
-	    for (int i = 0; i < goods.length; i++) {
-	        ans.append(goods[i].toString());
-	    }
-	    ans.append(']');
-	    return ans.toString();
-	}
+
+    /**
+     * toString method for PlanetMarket required by CodePro.
+     * 
+     * @return String representation of PlanetMarket.
+     */
+    public String toString() {
+        final StringBuffer ans = new StringBuffer("[");
+        for (int i = 0; i < goods.length; i++) {
+            ans.append(goods[i].toString());
+        }
+        ans.append(']');
+        return ans.toString();
+    }
 }
