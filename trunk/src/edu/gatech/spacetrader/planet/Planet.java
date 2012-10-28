ad682daa44725bf661a57f3a6e82e2f3d7854577
duelgamer <duelgamer@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-10-28 22:17:47 +0000

    Fixed display of fly screen. Added click checking for fly screen. (GH)

--- trunk/src/edu/gatech/spacetrader/planet/Planet.java
+++ trunk/src/edu/gatech/spacetrader/planet/Planet.java
@@ -6,6 +6,7 @@ package edu.gatech.spacetrader.planet;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -20,235 +21,130 @@ public class Planet {
     /**
      * Field PLANET_NAMES.
      */
-    private static final String[] PLANET_NAMES = {
-            "Acamar",
-            "Adahn",        // The alternate personality for The Nameless One in "Planescape: Torment"
-            "Aldea",
-            "Andevian",
-            "Antedi",
-            "Balosnee",
-            "Baratas",
-            "Brax",         // One of the heroes in Master of Magic
-            "Bretel",       // This is a Dutch device for keeping your pants up.
-            "Calondia",
-            "Campor",
-            "Capelle",      // The city I lived in while programming this game
-            "Carzon",
-            "Castor",       // A Greek demi-god
-            "Cestus",
-            "Cheron",       
-            "Courteney",    // After Courteney Cox
-            "Daled",
-            "Damast",
-            "Davlos",
-            "Deneb",
-            "Deneva",
-            "Devidia",
-            "Draylon",
-            "Drema",
-            "Endor",
-            "Esmee",        // One of the witches in Pratchett's Discworld
-            "Exo",
-            "Ferris",       // Iron
-            "Festen",       // A great Scandinavian movie
-            "Fourmi",       // An ant, in French
-            "Frolix",       // A solar system in one of Philip K. Dick's novels
-            "Gemulon",
-            "Guinifer",     // One way of writing the name of king Arthur's wife
-            "Hades",        // The underworld
-            "Hamlet",       // From Shakespeare
-            "Helena",       // Of Troy
-            "Hulst",        // A Dutch plant
-            "Iodine",       // An element
-            "Iralius",
-            "Janus",        // A seldom encountered Dutch boy's name
-            "Japori",
-            "Jarada",
-            "Jason",        // A Greek hero
-            "Kaylon",
-            "Khefka",
-            "Kira",         // My dog's name
-            "Klaatu",       // From a classic SF movie
-            "Klaestron",
-            "Korma",        // An Indian sauce
-            "Kravat",       // Interesting spelling of the French word for "tie"
-            "Krios",
-            "Laertes",      // A king in a Greek tragedy
-            "Largo",
-            "Lave",         // The starting system in Elite
-            "Ligon",
-            "Lowry",        // The name of the "hero" in Terry Gilliam's "Brazil"
-            "Magrat",       // The second of the witches in Pratchett's Discworld
-            "Malcoria",
-            "Melina",
-            "Mentar",       // The Psilon home system in Master of Orion
-            "Merik",
-            "Mintaka",
-            "Montor",       // A city in Ultima III and Ultima VII part 2
-            "Mordan",
-            "Myrthe",       // The name of my daughter
-            "Nelvana",
-            "Nix",          // An interesting spelling of a word meaning "nothing" in Dutch
-            "Nyle",         // An interesting spelling of the great river
-            "Odet",
-            "Og",           // The last of the witches in Pratchett's Discworld
-            "Omega",        // The end of it all
-            "Omphalos",     // Greek for navel
-            "Orias",
-            "Othello",      // From Shakespeare
-            "Parade",       // This word means the same in Dutch and in English
-            "Penthara",
-            "Picard",       // The enigmatic captain from ST:TNG
-            "Pollux",       // Brother of Castor
-            "Quator",
-            "Rakhar",
-            "Ran",          // A film by Akira Kurosawa
-            "Regulas",
-            "Relva",
-            "Rhymus",
-            "Rochani",
-            "Rubicum",      // The river Ceasar crossed to get into Rome
-            "Rutia",
-            "Sarpeidon",
-            "Sefalla",
-            "Seltrice",
-            "Sigma",
-            "Sol",          // That's our own solar system
-            "Somari",
-            "Stakoron",
-            "Styris",
-            "Talani",
-            "Tamus",
-            "Tantalos",     // A king from a Greek tragedy
-            "Tanuga",
-            "Tarchannen",
-            "Terosa",
-            "Thera",        // A seldom encountered Dutch girl's name
-            "Titan",        // The largest moon of Jupiter
-            "Torin",        // A hero from Master of Magic
-            "Triacus",
-            "Turkana",
-            "Tyrus",
-            "Umberlee",     // A god from AD&D, which has a prominent role in Baldur's Gate
-            "Utopia",       // The ultimate goal
-            "Vadera",
-            "Vagra",
-            "Vandor",
-            "Ventax",
-            "Xenon",
-            "Xerxes",       // A Greek hero
-            "Yew",          // A city which is in almost all of the Ultima games
-            "Yojimbo",      // A film by Akira Kurosawa
-            "Zalkon",
-            "Zuul"          // From the first Ghostbusters movie
-        };
-    
-    public static enum Event{
-    	DROUGHT, COLD, CROPFAIL, WAR, BOREDOM, PLAGUE, LACKOFWORKERS, NONE
+    private static final String[] PLANET_NAMES = { "Acamar", "Adahn", "Aldea",
+            "Andevian", "Antedi", "Balosnee", "Baratas", "Brax", "Bretel",
+            "Calondia", "Campor", "Capelle", "Carzon", "Castor", "Cestus",
+            "Cheron", "Courteney", "Daled", "Damast", "Davlos", "Deneb",
+            "Deneva", "Devidia", "Draylon", "Drema", "Endor", "Esmee", "Exo",
+            "Ferris", "Festen", "Fourmi", "Frolix", "Gemulon", "Guinifer",
+            "Hades", "Hamlet", "Helena", "Hulst", "Iodine", "Iralius", "Janus",
+            "Japori", "Jarada", "Jason", "Kaylon", "Khefka", "Kira", "Klaatu",
+            "Klaestron", "Korma", "Kravat", "Krios", "Laertes", "Largo",
+            "Lave", "Ligon", "Lowry", "Magrat", "Malcoria", "Melina", "Mentar",
+            "Merik", "Mintaka", "Montor", "Mordan", "Myrthe", "Nelvana", "Nix",
+            "Nyle", "Odet", "Og", "Omega", "Omphalos", "Orias", "Othello",
+            "Parade", "Penthara", "Picard", "Pollux", "Quator", "Rakhar",
+            "Ran", "Regulas", "Relva", "Rhymus", "Rochani", "Rubicum", "Rutia",
+            "Sarpeidon", "Sefalla", "Seltrice", "Sigma", "Sol", "Somari",
+            "Stakoron", "Styris", "Talani", "Tamus", "Tantalos", "Tanuga",
+            "Tarchannen", "Terosa", "Thera", "Titan", "Torin", "Triacus",
+            "Turkana", "Tyrus", "Umberlee", "Utopia", "Vadera", "Vagra",
+            "Vandor", "Ventax", "Xenon", "Xerxes", "Yew", "Yojimbo", "Zalkon",
+            "Zuul" };
+
+    public static enum Event {
+        DROUGHT, COLD, CROPFAIL, WAR, BOREDOM, PLAGUE, LACKOFWORKERS, NONE
     }
-    
+
     /**
      */
     public static enum Environment {
-    	DESERT(new Color(0xCD, 0x95, 0x0C)),
-    	WATER(Color.BLUE),
-    	JUNGLE(new Color(00, 0x64, 00)), 
-    	ICE(new Color(0x36, 0xDB, 0xCA)),
-    	EARTHLIKE(Color.CYAN),
-    	TROPICAL(Color.GREEN),
-    	UNDERGROUND(Color.BLACK);
-    	
-    	/**
+        DESERT(new Color(0xCD, 0x95, 0x0C)), WATER(Color.BLUE), JUNGLE(
+                new Color(00, 0x64, 00)), ICE(new Color(0x36, 0xDB, 0xCA)), EARTHLIKE(
+                Color.CYAN), TROPICAL(Color.GREEN), UNDERGROUND(Color.BLACK);
+
+        /**
          * Field color.
          */
-    	private final Color color;
-    		
-    	/**
-    	 * Constructor for Environment.
-    	 * 
-    	 * @param color Color.
+        private final Color color;
+
+        /**
+         * Constructor for Environment.
+         * 
+         * @param color
+         *            Color.
          */
-    	private Environment(Color color) {
-    	    this.color = color;
-    	}
-    	
-    	/**
+        private Environment(Color color) {
+            this.color = color;
+        }
+
+        /**
          * Method getColor.
          * 
          * @return Color.
          */
-    	public Color getColor(){
-    	    return color;
-    	}
-    
+        public Color getColor() {
+            return color;
+        }
+
     }
-    
+
     /**
      */
-    public static enum TechLevel{
-    	PRE_AGRICULTURAL(0), AGRICULTURAL(1), MEDIEVAL(2), RENAISSANCE(3),
-    	EARLY_INDUSTRIAL(4), INDUSTRIAL(5), POST_INDUSTRIAL(6), HI_TECH(7);
-    	
-    	/**
+    public static enum TechLevel {
+        PRE_AGRICULTURAL(0), AGRICULTURAL(1), MEDIEVAL(2), RENAISSANCE(3), EARLY_INDUSTRIAL(
+                4), INDUSTRIAL(5), POST_INDUSTRIAL(6), HI_TECH(7);
+
+        /**
          * Field LevelInt of TechLevel.
          */
-    	private final int levelInt;
-    	
-    	/**
-    	 * TechLevel constructor to set level int used in calculations.
-    	 * 
-    	 * @param levelInt
-    	 */
-    	private TechLevel(int levelInt) {
-    	    this.levelInt = levelInt;
-    	}
+        private final int levelInt;
 
-    	/**
-    	 * Get the levelInt of a TechLevel.
-    	 * @return levelInt.
-    	 */
+        /**
+         * TechLevel constructor to set level int used in calculations.
+         * 
+         * @param levelInt
+         */
+        private TechLevel(int levelInt) {
+            this.levelInt = levelInt;
+        }
+
+        /**
+         * Get the levelInt of a TechLevel.
+         * 
+         * @return levelInt.
+         */
         public int getTechLevel() {
             return levelInt;
         }
     }
-    
+
     /**
      * code pro is wrong here. this will change
      */
     private TechLevel techLevel;
-    
+
     /**
      * 
      */
     private Environment environment;
-    
+
     /**
      * 
      */
     @SuppressWarnings("unused")
-	private Event currentEvent;
-    
+    private Event currentEvent;
+
     /**
      * Field name.
      */
     private String name;
-    
+
     /**
      * Field NAMES_USED.
      */
     private static final ArrayList<Integer> NAMES_USED = new ArrayList<Integer>();
-    
+
     /**
      * Field RAND.
      */
     private static final Random RAND = new Random();
-    
+
     /**
-     * Field NUM_PLANETS.
-     * (value is 120)
+     * Field NUM_PLANETS. (value is 120)
      */
     private static final int NUM_PLANETS = 120;
-    
+
     /**
      * Field x.
      */
@@ -256,50 +152,53 @@ public class Planet {
      * Field y.
      */
     private int x, y;
-    
+
     /**
      * Field market.
      */
     private final PlanetMarket market;
-    
+
     /**
      * Constructor for Planet.
-     * @param galaxyWidth Width of galaxy.
-     * @param galaxyHeight Height of the galaxy.
+     * 
+     * @param galaxyWidth
+     *            Width of galaxy.
+     * @param galaxyHeight
+     *            Height of the galaxy.
      */
-    public Planet(int width, int galaxyWidth, int galaxyHeight){
+    public Planet(int width, int galaxyWidth, int galaxyHeight) {
         int nextPlanet = RAND.nextInt(NUM_PLANETS);
-        
+
         while (NAMES_USED.contains(nextPlanet)) {
             nextPlanet = RAND.nextInt(NUM_PLANETS);
         }
-        
+
         name = PLANET_NAMES[nextPlanet];
         NAMES_USED.add(nextPlanet);
-        
+
         x = RAND.nextInt(galaxyWidth);
         y = RAND.nextInt(galaxyHeight);
-        
-        techLevel = TechLevel.values()
-        		[RAND.nextInt(TechLevel.values().length)];
-        environment = Environment.values()[RAND.nextInt(Environment.values().length)];
-        
+
+        techLevel = TechLevel.values()[RAND.nextInt(TechLevel.values().length)];
+        environment = Environment.values()[RAND
+                .nextInt(Environment.values().length)];
+
         currentEvent = Event.NONE;
-        market = new PlanetMarket(this, (width - Galaxy.GALAXY_WIDTH + 10) / 2, 0);
+        market = new PlanetMarket(this, (width - Galaxy.GALAXY_WIDTH + 10) / 2,
+                0);
     }
-    
+
     /**
-     * This is the huge method that will generate random events, 
-     * advance or revert the civilization level,
-     * and determine market prices.
-     * It should be called whenever the player flies to a new location.
+     * This is the huge method that will generate random events, advance or
+     * revert the civilization level, and determine market prices. It should be
+     * called whenever the player flies to a new location.
      */
-    public void advanceTime(){
-    	//TODO - Everything
+    public void advanceTime() {
+        // TODO - Everything
         System.out.println("Time++");
         market.updatePrices();
     }
-    
+
     /**
      * Method getX.
      * 
@@ -308,102 +207,117 @@ public class Planet {
     public int getX() {
         return x;
     }
-    
+
     /**
      * Method getY.
+     * 
      * @return int Y value of planet.
      */
     public int getY() {
         return y;
     }
-    
+
     /**
      * @return The planet's environment type
      */
-    public Environment getEnvironment(){
-    	return this.environment;
+    public Environment getEnvironment() {
+        return this.environment;
     }
-    
+
     /**
      * @return The planet's civilization level
      */
-    public TechLevel getCivLevel(){
-    	return this.techLevel;
+    public TechLevel getCivLevel() {
+        return this.techLevel;
     }
-    
+
     /**
      * 
      */
     @SuppressWarnings("unused")
-    private void advanceCivilization(){ // $codepro.audit.disable unusedMethod
-    	//Advance civilization to next level
-    	//called internally when a random conditional is met during player movement.
+    private void advanceCivilization() { // $codepro.audit.disable unusedMethod
+        // Advance civilization to next level
+        // called internally when a random conditional is met during player
+        // movement.
         switch (techLevel) {
-            case PRE_AGRICULTURAL:
-                techLevel = TechLevel.AGRICULTURAL;
-                break;
-            case AGRICULTURAL:
-            	techLevel = TechLevel.MEDIEVAL;
-            	break;
-            case MEDIEVAL:
-            	techLevel = TechLevel.EARLY_INDUSTRIAL;
-            	break;
-            case EARLY_INDUSTRIAL:
-            	techLevel = TechLevel.INDUSTRIAL;
-            	break;
-            case INDUSTRIAL:
-            	techLevel = TechLevel.POST_INDUSTRIAL;
-            	break;
-            case POST_INDUSTRIAL:
-                techLevel = TechLevel.HI_TECH;
-                break;
-            default:
-                break;
+        case PRE_AGRICULTURAL:
+            techLevel = TechLevel.AGRICULTURAL;
+            break;
+        case AGRICULTURAL:
+            techLevel = TechLevel.MEDIEVAL;
+            break;
+        case MEDIEVAL:
+            techLevel = TechLevel.EARLY_INDUSTRIAL;
+            break;
+        case EARLY_INDUSTRIAL:
+            techLevel = TechLevel.INDUSTRIAL;
+            break;
+        case INDUSTRIAL:
+            techLevel = TechLevel.POST_INDUSTRIAL;
+            break;
+        case POST_INDUSTRIAL:
+            techLevel = TechLevel.HI_TECH;
+            break;
+        default:
+            break;
         }
     }
-    
+
     /**
-     * Method changeEvent.
-     * Changes the current event to a new random event.
+     * Method changeEvent. Changes the current event to a new random event.
      */
-    public void changeEvent(){
-        currentEvent = Event.values()[new Random().nextInt(Event.values().length)];
+    public void changeEvent() {
+        currentEvent = Event.values()[new Random()
+                .nextInt(Event.values().length)];
     }
-    
+
     /**
-     * @return A short descriptor of any recent planetary events that affect the market
+     * @return A short descriptor of any recent planetary events that affect the
+     *         market
      */
-    public String eventMessage(){
-    	/*TODO - Create a string-generator. More importantly,
-    	 * create a list (enum) of possible events,
-    	 * e.g. Government uprising, famine.
-    	 * Events can be triggered in the "advanceTime" method.
-    	 * Message is based on "currentEvent" field.
-    	 * The message will be displayed when a player arrives at a planet
-    	*/
-    	return null;
+    public String eventMessage() {
+        /*
+         * TODO - Create a string-generator. More importantly, create a list
+         * (enum) of possible events, e.g. Government uprising, famine. Events
+         * can be triggered in the "advanceTime" method. Message is based on
+         * "currentEvent" field. The message will be displayed when a player
+         * arrives at a planet
+         */
+        return null;
     }
-    
+
     /**
      * Method toString.
+     * 
      * @return String
      */
-    public String toString(){
+    public String toString() {
         return name;
     }
-    
+
+    public void drawMini(Graphics g, GamePanel panel, int width, int height) {
+        g.setColor(environment.getColor());
+        g.fillOval(width - Galaxy.HALF_GALAXY_WIDTH + (x - 2), height
+                - Galaxy.HALF_GALAXY_HEIGHT + (y - 2), 4, 4); // $codepro.audit.disable
+                                                              // numericLiterals
+        g.setColor(Color.BLACK);
+    }
+
     /**
      * Method draw.
      * 
-     * @param g Graphics
-     * @param panel GamePanel
-     * @param width Int
-     * @param height Int
+     * @param g
+     *            Graphics
+     * @param panel
+     *            GamePanel
+     * @param width
+     *            Int
+     * @param height
+     *            Int
      */
     public void draw(Graphics g, GamePanel panel, int width, int height) {
         g.setColor(environment.getColor());
-        g.fillOval(width - Galaxy.HALF_GALAXY_WIDTH + (x - 2),
-                    height - Galaxy.HALF_GALAXY_HEIGHT + (y - 2), 4, 4); // $codepro.audit.disable numericLiterals
+        g.fillOval((x * 6), (y * 5), 20, 20); // $codepro.audit.disable numericLiterals
         g.setColor(Color.BLACK);
     }
 
@@ -415,25 +329,30 @@ public class Planet {
     public PlanetMarket getMarket() {
         return market;
     }
-    
-    public void setX(int x){
-    	this.x = x;
+
+    public void setX(int x) {
+        this.x = x;
     }
-    
-    public void setY(int y){
-    	this.y = y;
+
+    public void setY(int y) {
+        this.y = y;
     }
-    
-    public void setEnvironment(String env){
-    		environment = Environment.valueOf(env);
+
+    public void setEnvironment(String env) {
+        environment = Environment.valueOf(env);
     }
-    
-    public void setCivLevel(String civ){
-    	techLevel = TechLevel.valueOf(civ);
+
+    public void setCivLevel(String civ) {
+        techLevel = TechLevel.valueOf(civ);
     }
-    
-    public void setName(String name){
-    	this.name = name;
+
+    public void setName(String name) {
+        this.name = name;
     }
-   
+
+    public boolean checkForClick(Point p) {
+        int dx = p.x - ((x * 6) + 10);
+        int dy = p.y - ((y * 5) + 10);
+        return dx * dx + dy * dy <= 10 * 10;
+    }
 }
--- trunk/src/edu/gatech/spacetrader/screens/FlyScreen.java
+++ trunk/src/edu/gatech/spacetrader/screens/FlyScreen.java
@@ -7,21 +7,24 @@ import java.awt.Graphics;
 import java.awt.Point;
 
 import edu.gatech.spacetrader.main.GamePanel;
+import edu.gatech.spacetrader.main.SpaceTrader;
+import edu.gatech.spacetrader.planet.Galaxy;
+import edu.gatech.spacetrader.planet.Planet;
 
 /**
  */
 public class FlyScreen extends Screen {
-    
+
     /**
      * Field gameScreen.
      */
     private GameScreen gameScreen;
-    
+
     /**
      * Field panel.
      */
     private GamePanel panel;
-    
+
     /**
      * Field width.
      */
@@ -32,10 +35,15 @@ public class FlyScreen extends Screen {
 
     /**
      * Constructor for FlyScreen.
-     * @param gameScreen GameScreen
-     * @param panel GamePanel
-     * @param width int
-     * @param height int
+     * 
+     * @param gameScreen
+     *            GameScreen
+     * @param panel
+     *            GamePanel
+     * @param width
+     *            int
+     * @param height
+     *            int
      */
     public FlyScreen(GameScreen gameScreen, GamePanel panel, int width,
             int height) {
@@ -47,25 +55,41 @@ public class FlyScreen extends Screen {
 
     /**
      * Method draw.
-     * @param g Graphics
+     * 
+     * @param g
+     *            Graphics
      */
     @Override
     public void draw(Graphics g) {
         gameScreen.getGalaxy().draw(g, panel, width, height);
+        g.fillOval((150 * 6) - 5, (100 * 5) - 5, 10, 10);
+        g.drawOval(
+                ((gameScreen.getCurrentPlanet().getX() * 6) + 10)
+                        - (gameScreen.getPlayer().getSpaceCraft().getSpeed() * 10),
+                ((gameScreen.getCurrentPlanet().getY() * 5) + 10)
+                        - (gameScreen.getPlayer().getSpaceCraft().getSpeed() * 10),
+                gameScreen.getPlayer().getSpaceCraft().getSpeed() * 20,
+                gameScreen.getPlayer().getSpaceCraft().getSpeed() * 20);
     }
 
     /**
      * Method checkForClick.
-     * @param point Point
+     * 
+     * @param point
+     *            Point
      */
     @Override
     public void checkForClick(Point point) {
-        // TODO Auto-generated method stub
-        System.out.println("Clicked");
+        for (Planet p : gameScreen.getGalaxy().getPlanets()) {
+            if (p.checkForClick(point)) {
+                System.out.println("Hello!");
+            }
+        }
     }
 
     /**
      * Method toString.
+     * 
      * @return String
      */
     @Override
