c1ff55ade59dfb25dde8e07631b304735aa3b985
duelgamer <duelgamer@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-10-29 17:39:46 +0000

    Gray out area of Fly Screen that you can't fly to. (GH)

--- src/edu/gatech/spacetrader/screens/FlyScreen.java
+++ src/edu/gatech/spacetrader/screens/FlyScreen.java
@@ -3,10 +3,14 @@
  */
 package edu.gatech.spacetrader.screens;
 
+import java.awt.AlphaComposite;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.RenderingHints;
 import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
 
 import edu.gatech.spacetrader.main.GamePanel;
 import edu.gatech.spacetrader.planet.Planet;
@@ -35,6 +39,8 @@ public class FlyScreen extends Screen {
 
     private Ellipse2D.Double range;
 
+    private BufferedImage img;
+
     /**
      * Constructor for FlyScreen.
      * 
@@ -53,13 +59,36 @@ public class FlyScreen extends Screen {
         this.panel = panel;
         this.width = width;
         this.height = height;
+
+        int ovalX = (gameScreen.getCurrentPlanet().getX() * 5) + 10;
+        int ovalY = (gameScreen.getCurrentPlanet().getY() * 5) + 10;
+        int ovalRadius = ((gameScreen.getPlayer().getSpaceCraft().getSpeed() + gameScreen
+                .getPlayer().getSkillsArray()[0] / 2) * (10 * 5)) / 2;
+
         range = new Ellipse2D.Double(
-                ((gameScreen.getCurrentPlanet().getX() * 6) + 10)
-                        - ((gameScreen.getPlayer().getSpaceCraft().getSpeed() + gameScreen.getPlayer().getSkillsArray()[0] / 2) * (5 * 6)),
-                ((gameScreen.getCurrentPlanet().getY() * 5) + 10)
-                        - ((gameScreen.getPlayer().getSpaceCraft().getSpeed() + gameScreen.getPlayer().getSkillsArray()[0] / 2) * (5 * 5)),
-                (gameScreen.getPlayer().getSpaceCraft().getSpeed() + gameScreen.getPlayer().getSkillsArray()[0] / 2) * (10 * 6),
-                (gameScreen.getPlayer().getSpaceCraft().getSpeed() + gameScreen.getPlayer().getSkillsArray()[0] / 2) * (10 * 5));
+                ovalX - ((gameScreen.getPlayer().getSpaceCraft().getSpeed() 
+                        + gameScreen.getPlayer().getSkillsArray()[0] / 2) * (5 * 5)),
+                ovalY - ((gameScreen.getPlayer().getSpaceCraft().getSpeed()
+                        + gameScreen.getPlayer().getSkillsArray()[0] / 2) * (5 * 5)),
+                2 * ovalRadius, 2 * ovalRadius);
+
+        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = img.createGraphics();
+
+        /* Draw the grey rectangle */
+        g.setColor(new Color(180, 180, 180, 200));
+        g.fillRect(0, 0, width, height);
+
+        /* Enable Anti-Alias */
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        /* Clear the circle away */
+        g.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR, 1.0f));
+        g.fillOval(ovalX - ovalRadius, ovalY - ovalRadius, 2 * ovalRadius,
+                2 * ovalRadius);
+
+        g.dispose();
     }
 
     /**
@@ -71,9 +100,10 @@ public class FlyScreen extends Screen {
     @Override
     public void draw(Graphics g) {
         gameScreen.getGalaxy().draw(g, panel, width, height);
-        g.fillOval((150 * 6) - 5, (100 * 5) - 5, 10, 10);
+        g.fillOval((150 * 5) - 5, (100 * 5) - 5, 10, 10);
         Graphics2D g2 = (Graphics2D) g;
         g2.draw(range);
+        g.drawImage(img, 0, 0, null);
     }
 
     /**
