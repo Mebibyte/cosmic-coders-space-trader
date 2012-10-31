049d704aa1736ea6b4ba757e6d5546fd45fb281d
duelgamer <duelgamer@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-10-31 06:18:19 +0000

    Added better fuel indicator. Changed fuel used to be based on distance. Added buy fuel button (10 fuel for 5 credits). (GH)

--- trunk/src/edu/gatech/spacetrader/spacecraft/SpaceCraft.java
+++ trunk/src/edu/gatech/spacetrader/spacecraft/SpaceCraft.java
@@ -4,6 +4,7 @@
 
 package edu.gatech.spacetrader.spacecraft;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.util.Random;
@@ -43,20 +44,20 @@ public abstract class SpaceCraft {
      */
     protected ImageIcon shipIcon;
 
-    public SpaceCraft(int health, int speed, int attack,
-            int defense, int maxCapacity, Good[] storage) {
+    public SpaceCraft(int health, int speed, int attack, int defense,
+            int maxCapacity, Good[] storage) {
         this.health = health;
         this.speed = speed;
         this.attack = attack;
         this.defense = defense;
         this.storage = storage;
         this.maxCapacity = maxCapacity;
-        fuel = 10;
-        
+        fuel = 100;
+
         int goodCount = 0;
         int x = ((SpaceTrader.WIDTH - Galaxy.GALAXY_WIDTH + 10) / 2) + 300;
         int y = 0;
-        
+
         for (int i = y; i < (y + 150); i += 75) {
             for (int j = x; j < (x + 250); j += 50) {
                 this.storage[goodCount] = new Good(goodCount, this, j, i);
@@ -74,8 +75,8 @@ public abstract class SpaceCraft {
      * Method fly.
      * 
      */
-    public void fly() {
-        fuel--;
+    public void fly(int distance) {
+        fuel -= distance / speed;
     }
 
     /**
@@ -122,7 +123,7 @@ public abstract class SpaceCraft {
     public boolean canAddToStorage() {
         return quantity < maxCapacity;
     }
-    
+
     public void addToStorage(Good g) {
         storage[g.getIndex()].addGood();
         quantity++;
@@ -152,13 +153,20 @@ public abstract class SpaceCraft {
     public int getSpeed() {
         return speed;
     }
-    
+
     public int getFuel() {
         return fuel;
     }
-    
-    public void addFuel() {
-        fuel++;
+
+    public boolean addFuel() {
+        if (fuel < 100) {
+            fuel += 10;
+            if (fuel > 100) {
+                fuel = 100;
+            }
+            return true;
+        }
+        return false;
     }
 
     public Good goodClicked(Point point) {
@@ -178,5 +186,31 @@ public abstract class SpaceCraft {
         for (int i = 0; i < 10; i++) {
             storage[i].setSalePrice(market.getGood(i).getSellPrice());
         }
+    }
+
+    public void drawFuel(Graphics g, GamePanel panel, int x, int y) {
+        g.setColor(Color.BLACK);
+        g.fillRect(x, y, 25, -1 * (fuel - 100));
+        g.setColor(Color.ORANGE);
+        g.fillRect(x, y + -1 * (fuel - 100), 25, 100 - -1 * (fuel - 100));
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, 25, 100);
+
+        for (int i = y; i < (y + 100); i += 5) {
+            if (i % 10 == 0) {
+                g.drawLine(x, i, x + 5, i);
+            } else {
+                g.drawLine(x, i, x + 3, i);
+            }
+        }
+
+        g.drawString("F", (25 / 2) - (g.getFontMetrics().stringWidth("F") / 2),
+                y + g.getFontMetrics().getHeight());
+        g.drawString("E", (25 / 2) - (g.getFontMetrics().stringWidth("E") / 2),
+                y + 95);
+    }
+
+    public boolean canFly() {
+        return fuel > 0;
     }
 }
