8db41508cdd384e1c701dcd04435fc55dfd19b28
duelgamer <duelgamer@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-11-12 06:37:00 +0000

    Added JUnit for M10. (GH)

---
+++ tests/SpaceTraderTests.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SpaceTraderTests {
+
+    @Test
+    public void SpaceTraderTest1() {
+        assertTrue("True?", true);
+    }
+
+}
