e714fd482eeedd0b823c87e211389fdd404725e7
duelgamer <duelgamer@7284f1b1-e371-e299-e0a6-3945f7e4505c>
2012-11-13 17:00:02 +0000

    Created my JUnit test. (GH)

--- tests/SpaceTraderTests.java
+++ tests/SpaceTraderTests.java
@@ -4,11 +4,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.gatech.spacetrader.spacecraft.SpaceCraft;
+import edu.gatech.spacetrader.spacecraft.Gnat;
+import edu.gatech.spacetrader.good.Good;
+
 public class SpaceTraderTests {
 
     @Test
-    public void SpaceTraderTest1() {
-        assertTrue("True?", true);
+    public void AddingToCraftTest() { // By Glenn
+        SpaceCraft craft = new Gnat();
+        assertTrue("Can add to empty storage?", craft.canAddToStorage());
+        for (int i = 0; i < 10; i++) {
+            craft.addToStorage(new Good(0, craft, 0, 0));
+        }
+        assertFalse("Can't add to full storage?", craft.canAddToStorage());
     }
 
 }
